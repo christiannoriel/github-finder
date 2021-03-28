b2784081b20e77a13f171c5fee3d3813c69c8ef8
Chris Orange <chrisorange@Chriss-MacBook-Pro.local>
2021-03-28 13:36:42 +0800

    Add HTTP request and update state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+    // console.log(res.data);
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,34 +2,12 @@ import React, { Component } from 'react'
 import UserItems from './UserItem'
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        "login": "mojombo",
-        "id": 1,
-        "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
-        "html_url": "https://github.com/mojombo",
-      },
-      {
-        "login": "defunkt",
-        "id": 2,
-        "avatar_url": "https://avatars.githubusercontent.com/u/2?v=4",
-        "html_url": "https://github.com/defunkt",
-      },
-      {
-        "login": "pjhyett",
-        "id": 3,
-        "avatar_url": "https://avatars.githubusercontent.com/u/3?v=4",
-        "html_url": "https://github.com/pjhyett",
-      }
-    ]
-  };
 
   render() {
     return (
       <div style={userStyle}>
         {
-          this.state.users.map((user) => (
+          this.props.users.map((user) => (
             <UserItems key={user.id} user={user} />
           ))
         }
