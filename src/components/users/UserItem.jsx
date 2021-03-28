88b3422d28062d07e4cbefa1c097ad89ad005edc
Chris Orange <chrisorange@Chriss-MacBook-Pro.local>
2021-03-28 12:56:09 +0800

    List and Passing with Props

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,19 +1,10 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo'
-    }
-  }
 
   render() {
 
-    const { avatar_url, login, html_url } = this.state;
+    const { avatar_url, login, html_url } = this.props.user;
     return (
       <div className="card text-center">
         <img src={avatar_url} className="round-img" alt="" style={{ width: '60px' }} />
