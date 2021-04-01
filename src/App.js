756673df2aebb58c41c7eb0273361361dc240e67
Chris Orange <chrisorange@Chriss-MacBook-Pro.local>
2021-04-01 15:00:54 +0800

    Create reducer and actions

--- src/App.js
+++ src/App.js
@@ -26,16 +26,6 @@ const App = () => {
   //   // console.log(res.data);
   // };
 
-  // Search Github users (props from Search.js)
-  const searchUsers = async (text) => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // Get single Github user
   const getUser = async (username) => {
     setLoading(true);
@@ -81,7 +71,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
--- src/context/github/GithubState.jsx
+++ src/context/github/GithubState.jsx
@@ -20,7 +20,17 @@ const GithubState = (props) => {
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  //Search Users
+  // Search Github users (props from Search.js)
+  const searchUsers = async (text) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
 
   //Get Users
 
@@ -29,6 +39,7 @@ const GithubState = (props) => {
   //Clear Users
 
   //Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -37,6 +48,7 @@ const GithubState = (props) => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
       }}>
       {props.children}
     </GithubContext.Provider>
